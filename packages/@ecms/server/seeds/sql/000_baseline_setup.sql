4a9f1e089bce18708522d5f457ef16018880da1b
Kishan <kishansambhi@hotmail.co.uk>
2021-10-03 22:35:24 +0100

    Add 000 script for DB setup Does whatever bootstrapping is needed before the main set of tables can be created.

---
+++ packages/@ecms/server/seeds/sql/000_baseline_setup.sql
@@ -0,0 +1,6 @@
+-- This script sets us the Postgres DB with the features we need to create our database
+-- It does NOT create the DB for us
+
+CREATE EXTENSION IF NOT EXISTS "ltree";
+
+-- TODO: User creation.
