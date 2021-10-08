a727fc3f69d48d5d26725b7b0a13e626103c9c73
Kishan <kishansambhi@hotmail.co.uk>
2021-10-08 23:25:43 +0100

    Adjust installed_modules.sql Move some stuff to ENUMs

--- packages/@ecms/server/seeds/sql/000_baseline_setup.sql
+++ packages/@ecms/server/seeds/sql/000_baseline_setup.sql
@@ -3,6 +3,10 @@
 
 CREATE EXTENSION IF NOT EXISTS "ltree";
 
+-- NOTE: COMMENT ON ____ is used as the tool we use to convert the DB
+-- to TypeScript interfaces can use the COMMENT ON metadata to add
+-- JSDoc comments to the generates interfaces.
+
 -- ENUM to refer to a event_and_group type
 -- these can either be an "event" or "group"
 -- The ENUM allos us to restrict values to just event or group when inserting or querying fields that should either have "event" or "group" as a value
@@ -24,4 +28,39 @@ COMMENT ON TYPE trackable_data IS 'The data an event can track, crucial in deter
 "individual": Individual performance of competitor information is tracked.
 "none": no data is tracked.';
 
+-- Module types
+-- This ENUM governs the types of extensions there can be
+-- Currently there are only two:
+-- 1. dataSource - these are extensions that provide sources of data for charity events,
+-- to collect things like how much money has been raised from a source of data like JustGiving
+-- or Eventbrite
+-- 2. pointsSystem - extensions that contain the logic to calculate points teams or competitors have acheived
+-- These are included in the ecms.type field of the package.json (extensions are written as node.js modules)
+CREATE TYPE extension_types AS ENUM (
+	'dataSource',
+	'pointsSystem'
+);
+COMMENT ON TYPE extension_types IS 'Module types - this ENUM governs the types of extensions there can be.
+
+Currently there are only two:
+1. dataSource - these are extensions that provide sources of data for charity events,
+to collect things like how much money has been raised from a source of data like JustGiving
+or Eventbrite
+
+2. pointsSystem - extensions that contain the logic to calculate points teams or competitors have acheived
+These are included in the ecms.type field of the package.json (extensions are written as node.js modules)';
+
+-- Install sources for extensions:
+-- - npm (installed from npm registry)
+-- - git (installed from git repo, using `npm install git+gitrepoURL`)
+-- - local (installed from a local folder, usually using `npm link path/to/folder`)
+CREATE TYPE extension_install_sources AS ENUM (
+	'npm',
+	'git',
+	'local'
+);
+COMMENT ON TYPE extension_install_sources IS 'Install sources for extensions:
+- npm (installed from npm registry)
+- git (installed from git repo, using `npm install git+gitrepoURL`)
+- local (installed from a local folder, usually using `npm link path/to/folder`)';
 -- TODO: User creation.
--- packages/@ecms/server/seeds/sql/installed_modules.sql
+++ packages/@ecms/server/seeds/sql/installed_modules.sql
@@ -4,23 +4,24 @@
 
 -- ************************************** "public".installed_modules
 
+CREATE 
+
 CREATE TABLE "public".installed_modules
 (
  module_id       uuid NOT NULL DEFAULT gen_random_uuid(),
  package_name    text NOT NULL,
- type            text NOT NULL,
+ type            extension_types NOT NULL, -- see 000_baseline_setup.sql for type
  ecmsObject      jsonb NOT NULL,
  version         text NOT NULL,
  npm_install_arg text NOT NULL,
- installed_from  text NOT NULL,
+ installed_from  extension_install_sources NOT NULL, -- see 000_baseline_setup.sql for type 
  CONSTRAINT PK_installed_modules PRIMARY KEY ( module_id )
 );
 
 COMMENT ON TABLE "public".installed_modules IS 'Contains an index of the modules installed by the system.';
 
-COMMENT ON COLUMN "public".installed_modules.module_id IS 'AUTOINCREMENT';
 COMMENT ON COLUMN "public".installed_modules.package_name IS 'NPM Package Name. e.g @ecms/ext-points-thresholds, from its package.json';
-COMMENT ON COLUMN "public".installed_modules.type IS 'ENUM: dataSource or pointsSystem';
+COMMENT ON COLUMN "public".installed_modules.type IS 'ENUM: dataSource or pointsSystem (see baseline setup)';
 COMMENT ON COLUMN "public".installed_modules.ecmsObject IS 'ECMS Object from package.json';
 COMMENT ON COLUMN "public".installed_modules.npm_install_arg IS 'What was entered into npm to install the package.';
 COMMENT ON COLUMN "public".installed_modules.installed_from IS 'Where it was installed from.
