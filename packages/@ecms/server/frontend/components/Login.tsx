718d533f04ef85f20a462934ec6907795fea9949
Kishan <kishansambhi@hotmail.co.uk>
2021-10-18 21:19:00 +0100

    Add Fonts

--- packages/@ecms/server/frontend/components/Login.tsx
+++ packages/@ecms/server/frontend/components/Login.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import CHBBlurredBG from "./AcrylicBackground";
+/// @ts-ignore
+import googleSignIn from "../assets/google/btn_google_signin_dark_normal_web@2x.png";
 
 /**
  * Root code for the Login Page - contains the entire login page
@@ -10,7 +12,9 @@ export default class LoginPage extends React.Component {
 			<CHBBlurredBG>
 				<div className="flexbox">
 					<div className="login-container">
-
+						<h1 className="title-header">ECMS</h1>
+						<a><img src={googleSignIn} className="google-sign-in" /></a>
+						<a className="ecms-link local-auth-activate">Use a username or password instead</a>
 					</div>
 				</div>
 			</CHBBlurredBG>
