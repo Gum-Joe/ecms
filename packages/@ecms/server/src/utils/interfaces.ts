aa679ae5ce7ba27aea96c8798f305fc926134465
Gum-Joe <kishansambhi@hotmail.co.uk>
2021-12-29 18:31:27 +0000

    Finish setup function

--- packages/@ecms/server/src/utils/interfaces.ts
+++ packages/@ecms/server/src/utils/interfaces.ts
@@ -3,6 +3,7 @@
  * @packageDocumentation
  */
 import { APIMessage } from "@ecms/api";
+import { ReqUploadCompetitorsCSV, SetupStates } from "@ecms/api/setup";
 import type { Request, Response } from "express";
 
 /**
@@ -14,4 +15,20 @@ export type RequestWithBody<T> = Request<Record<string, string>, any, Partial<T>
  * Allow us to respond with either the intended response (if code 200), or an error message
  * @template T the type of the intended response if the server returns a 200 code.
  */
-export type ECMSResponse<T = undefined> = T extends undefined ? Response<APIMessage> : Response<T | APIMessage>;
+export type ECMSResponse<T = undefined> = T extends undefined ? Response<APIMessage> : Response<T | APIMessage>
+
+/**
+ *  What's stored in Redis when importing competitors
+ */
+export type RedisCompetitorImport = Record<keyof ReqUploadCompetitorsCSV, string>
+
+/**
+ * Partial setup in redis when setup is running
+ */
+export interface PartialSetup {
+	status: SetupStates;
+	/** JSON setup data of form {@link SetupEventOrGroup} */
+	data: string;
+	// If set, means we need to grab a CSV.
+	hasImportedCompetitors?: boolean;
+}
