636d24e61554b2cec8d3caaeefb8100fc1272ae9
Gum-Joe <kishansambhi@hotmail.co.uk>
2022-01-09 21:32:43 +0000

    Add new components

--- packages/@ecms/server/frontend/components/fluent.ts
+++ packages/@ecms/server/frontend/components/fluent.ts
@@ -3,14 +3,19 @@
  * FROM https://www.fast.design/docs/integrations/react
  */
 import React from "react";
-import { fluentTab, fluentTabs, fluentTabPanel, provideFluentDesignSystem } from "@fluentui/web-components";
-import { provideFASTDesignSystem, fastCheckbox } from "@microsoft/fast-components";
+import { fluentTab, fluentTabs, fluentTabPanel, provideFluentDesignSystem, fluentTreeView, fluentTreeItem } from "@fluentui/web-components";
+import { provideFASTDesignSystem, fastCheckbox, fastTreeView } from "@microsoft/fast-components";
 import { provideReactWrapper } from "@microsoft/fast-react-wrapper";
 const { wrap } = provideReactWrapper(React, provideFASTDesignSystem());
+const { wrap: wrapFluent } = provideReactWrapper(React, provideFluentDesignSystem());
 
 export default wrap;
 
 export const FluentCheckbox = wrap(fastCheckbox());
+// @ts-expect-error: Bad typings!
+export const FluentTreeView = wrapFluent(fluentTreeView());
+// @ts-expect-error: Bad typings!
+export const FluentTreeItem = wrapFluent(fluentTreeItem());
 
 // also register tabs:
 // From https://docs.microsoft.com/en-us/fluent-ui/web-components/components/tabs
@@ -18,5 +23,7 @@ provideFluentDesignSystem()
 	.register(
 		fluentTab(),
 		fluentTabPanel(),
-		fluentTabs()
+		fluentTabs(),
+		fluentTreeView(),
+		fluentTreeItem(),
 	);
